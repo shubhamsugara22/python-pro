830bb9477ee093cd89ae893874fdf0c6d81826ec
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-12-24 19:28:04 +0530

    senti

---
+++ tweet.py
@@ -0,0 +1,108 @@
+# twitter app name sugara22
+
+import re
+
+import tweepy
+
+from tweepy import OAuthHandler
+
+from textblob import TextBlob
+
+
+class TwitterClient(object):
+
+    def __init__(self):
+        '''
+        Generic twitter for sentiment analysis
+
+        '''
+        consumer_key = 'BCR9If8x5kRoloTYqSOHyXBuv'
+        consumer_secret = 'muW3A2HdSRN8hMDBILsiE3371cbn5s8X0tYtESnLtqKT3KPsYA'
+        access_token = '940155094414135298-fyGuEHtDoxblwdef6RAeYljiqgiLD2a'
+        access_token_secret = '860IrMeAY7IMt1t5wLO4oVafmGWhgFgRnxlZKg2Trlfhk'
+
+        try:
+
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
+            self.api = tweepy.API(self.auth)
+
+        except:
+            print("Authentication  failure")
+
+    def clean_tweet(self, tweet):
+
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def get_tweet_sentiment(self, tweet):
+
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return "Positive"
+        elif analysis.sentiment.polarity == 0:
+            return "Neutral"
+        else:
+            return "Negative"
+
+    def get_tweets(self, query, count=10):
+
+        tweets = []
+
+        try:
+
+            fetched_tweets = self.api.search(q=query, count=count)
+
+            for tweet in fetched_tweets:
+
+                parsed_tweet = {}
+
+                parsed_tweet["text"] = tweet.text
+
+                parsed_tweet["sentiment"] = self.get_tweet_sentiment(
+                    tweet.text)
+
+                if tweet.retweet_count > 0:
+
+                    if parsed_tweet not in tweets:
+
+                        tweets.append(parsed_tweet)
+                    else:
+                        tweets.append(parsed_tweet)
+
+            return tweets
+
+        except tweepy.TweepError as e:
+
+            print("Error : " + str(e))
+
+
+def main():
+
+    api = TwitterClient()
+
+    tweets = api.get_tweets(query="Donald trump ", count=200)
+
+    ptweets = [tweet for tweet in tweets if tweet["sentiment"] == 'positive']
+
+    print("Positive tweet percentage : {} %".format(
+        100*len(ptweets)/len(tweets)))
+
+    ntweets = [tweet for tweet in tweets if tweet["sentiment"] == 'negative']
+
+    print("Negative tweet percentage : {} %".format(
+        100*len(ntweets)/len(tweets)))
+
+    print("Neutral  tweet percentage : {} % \ " .format(
+        100*(len(tweets)-(len(ntweets)+len(ptweets)))/len(tweets)))
+
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:10]:
+        print(tweet["text"])
+
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:10]:
+        print(tweet["text"])
+
+    if __name__ == "__main__":
+        main()
