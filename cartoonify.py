c9c580059394d0c1fe541632a3d7e6d49c159165
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-12-30 23:23:54 +0530

    cartoonify

---
+++ cartoonify.py
@@ -0,0 +1,21 @@
+import cv2
+
+import easygui
+
+import numpy
+
+import imageio
+
+import sys
+
+import matplotlib.pyplot as plt
+
+import os
+
+import tkinter as tk
+
+from tkinter import filedialog
+
+from tkinter import *
+
+from PIL import ImageTk, Image
