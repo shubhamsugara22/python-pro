89e34d32e31b5108ce0de5ea6143f7c006b981db
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-12-31 00:10:43 +0530

    cartoonify

--- cartoonify.py
+++ cartoonify.py
@@ -1,10 +1,10 @@
-import cv2
+import cv2  # used fro image processing
+from cv2 import *
+import easygui  # to open fileobx
 
-import easygui
+import numpy as np  # to store image
 
-import numpy
-
-import imageio
+import imageio  # to read iamge stored at particular path
 
 import sys
 
@@ -19,3 +19,26 @@ from tkinter import filedialog
 from tkinter import *
 
 from PIL import ImageTk, Image
+
+'''fileopen box to open the box to choose file
+   and help us store file path as string '''
+
+
+def upload():
+    ImagePath = easygui.fileopenbox()
+    cartoonify(ImagePath)
+
+
+def cartoonify(ImagePath):
+    originalImage = cv2.imread(ImagePath)
+    originalImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
+
+    # print(image)
+
+    if originalImage is None:
+        print("cannot find any image .Choose appropriate file")
+        sys.exit()
+
+    Resized1 = cv2.resize(originalImage, (960, 540))
+
+    #plt.imshow(Resized1,cmap = gray)
