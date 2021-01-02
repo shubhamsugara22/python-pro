de7b5e1b657ee2cdaaa3b030f0ed58ab43871cf8
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-02 12:15:17 +0530

    p1 chatbot

--- chatbot.py
+++ chatbot.py
@@ -12,3 +12,37 @@ import pickle
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
+
+words = []
+
+classes = []
+
+documents = []
+
+ignore_words = ['?', '!']
+
+data_file = open('intents.json').read()
+
+intents = json.load(data_file)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        # tokenize each word
+
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        # add dcoument in corpus
+        documents.append((w, intent['tag']))
+
+        # add to our classes  list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# lemmatize , lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower())
+         for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# sort clasess
+classes = sorted(list(set(classes)))
