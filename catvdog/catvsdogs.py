3a926d6a5b032b64adcdcc494c9869935b86b8c2
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-06 22:41:26 +0530

    cat vs dog

---
+++ catvdog/catvsdogs.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import pandas as pd
+
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.utils import to_categorical
+
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+
+import random
+
+import os
