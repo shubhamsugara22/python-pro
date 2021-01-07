9ebeae4476767e95fdd3284ad732ed9dd0828e7d
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-07 15:40:58 +0530

    Model start

--- catvdog/catvsdogs.py
+++ catvdog/catvsdogs.py
@@ -1,3 +1,5 @@
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+from keras.models import Sequential
 import numpy as np
 
 import pandas as pd
@@ -12,3 +14,50 @@ import matplotlib.pyplot as plt
 import random
 
 import os
+# image properties
+
+Image_Width = 128
+Image_Height = 128
+Image_Size = (Image_Width, Image_Height)
+Image_Channels = 3
+
+# preparing data set for model
+filenames = os.listdir("/train")
+
+categories = []
+
+for f_name in filenames:
+    category = f_name.split('.')[0]
+    if category == 'dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories,
+})
+
+# prepare model
+model = Sequential()
+
+model.add()
+model.add()
+model.add()
+model.add()
+
+model.add()
+model.add()
+model.add()
+model.add()
+
+model.add()
+model.add()
+model.add()
+model.add()
+
+model.add()
+model.add()
+model.add()
+model.add()
+model.add()
