b20f3bafd5095ea46a83b064061dab9bd1a2f394
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-08 12:41:20 +0530

    Neural Model created

--- catvdog/catvsdogs.py
+++ catvdog/catvsdogs.py
@@ -41,23 +41,27 @@ df = pd.DataFrame({
 # prepare model
 model = Sequential()
 
-model.add()
-model.add()
-model.add()
-model.add()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(
+    Image_Width, Image_Height, Image_Channels)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model.add()
-model.add()
-model.add()
-model.add()
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model.add()
-model.add()
-model.add()
-model.add()
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model.add()
-model.add()
-model.add()
-model.add()
-model.add()
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop', metrics=['accuracy'])
