fa11df178c1b4ea9f1390bc738e0c8b94e5bc4eb
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-21 23:55:39 +0530

    alarm set

--- pro.py
+++ pro.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import datetime
 import time
-import winsound
+#import winsound
+import pyaudio
+import wave
 
 
 def alarm(set_alarm_timer):
@@ -14,8 +16,44 @@ def alarm(set_alarm_timer):
         print(now)
         if now == set_alarm_timer:
         print("time to wake up")
-        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+        #winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+        filename = 'myfile.wav'
+
+        # Set chunk size of 1024 samples per data frame
+        chunk = 1024
+
+        # Open the sound file
+        wf = wave.open(filename, 'rb')
+
+        # Create an interface to PortAudio
+        p = pyaudio.PyAudio()
+
+        # Open a .Stream object to write the WAV file to
+        # 'output = True' indicates that the sound will be played rather than recorded
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        # Read data in chunks
+        data = wf.readframes(chunk)
+
+        # Play the sound by writing the audio data to the stream
+
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(chunk)
+
+        # Close and terminate the stream
+        stream.close()
+        p.terminate()
+
         break
+
+
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
 
 
 window = tk.Tk()
