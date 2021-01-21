d85b518ba9b4b3c1d8b978135f4135d95c296dc8
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-21 23:34:30 +0530

    alarm

--- pro.py
+++ pro.py
@@ -1,5 +1,26 @@
 import tkinter as tk
+import datetime
+import time
+import winsound
+
+
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print("the set date is :" date)
+        print(now)
+        if now == set_alarm_timer:
+        print("time to wake up")
+        winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+        break
+
 
 window = tk.Tk()
 
 window.geometry("800x600")
+
+
+window.mainloop()
