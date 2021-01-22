e2dea4cebe6cbace886cad3278a1166f14e80826
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-22 13:06:02 +0530

    Working alarm

--- pro.py
+++ pro.py
@@ -17,39 +17,32 @@ def alarm(set_alarm_timer):
         print(now)
         if now == set_alarm_timer:
             print("time to wake up")
-        #winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
-        filename = 'myfile.wav'
-
-        # Set chunk size of 1024 samples per data frame
-        chunk = 1024
-
-        # Open the sound file
-        wf = wave.open(filename, 'rb')
-
-        # Create an interface to PortAudio
-        p = pyaudio.PyAudio()
-
-        # Open a .Stream object to write the WAV file to
-        # 'output = True' indicates that the sound will be played rather than recorded
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
-
-        # Read data in chunks
-        data = wf.readframes(chunk)
-
-        # Play the sound by writing the audio data to the stream
-
-        while data != '':
-            stream.write(data)
+            #winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+            filename = 'myfile.wav'
+            # Set chunk size of 1024 samples per data frame
+            chunk = 1024
+            # Open the sound file
+            wf = wave.open(filename, 'rb')
+            # Create an interface to PortAudio
+            p = pyaudio.PyAudio()
+            # Open a .Stream object to write the WAV file to
+            # 'output = True' indicates that the sound will be played rather than recorded
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
+            # Read data in chunks
             data = wf.readframes(chunk)
+            # Play the sound by writing the audio data to the stream
+            while data != '':
+                stream.write(data)
+                data = wf.readframes(chunk)
 
-        # Close and terminate the stream
-        stream.close()
-        p.terminate()
+         # Close and terminate the stream
+            stream.close()
+            p.terminate()
 
-        break
+            break
 
 
 def actual_time():
