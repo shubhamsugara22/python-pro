137e9c4225611a4ef5f9f81fab4e41e99c47f6e0
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-23 15:51:32 +0530

    interface done

--- phonebook.py
+++ phonebook.py
@@ -4,3 +4,21 @@ root = Tk()
 
 root.geometry('400x400')
 root.config(bg='SlateGray3')
+root.resizable(0, 0)
+root.title("Address book")
+
+contact_list[
+
+]
+
+Name = StringVar()
+Number = StringVar()
+
+frame = Frame(root)
+frame.pack(side=RIGHT)
+
+scroll = Scrollbar(frame, orient=VERTICAL)
+select = Listbox(frame, yscrollcommand=scroll.set, height=12)
+scroll.config(command=select.yview)
+scroll.pack(side=RIGHT, fill=Y)
+select.pack(side=LEFT, fill=BOTH, expand=1)
