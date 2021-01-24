743b2ead5bd592ca60d7197d88d571fdfdc336a5
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-24 16:48:17 +0530

    Logic added

--- phonebook.py
+++ phonebook.py
@@ -8,7 +8,9 @@ root.resizable(0, 0)
 root.title("Address book")
 
 contact_list[
-
+    ['Shubham', '8944653345'],
+    ['shakuni', '666666666'],
+    ['fomo', '5698356784'],
 ]
 
 Name = StringVar()
@@ -22,3 +24,47 @@ select = Listbox(frame, yscrollcommand=scroll.set, height=12)
 scroll.config(command=select.yview)
 scroll.pack(side=RIGHT, fill=Y)
 select.pack(side=LEFT, fill=BOTH, expand=1)
+
+
+def Selected():
+    return int(select.curselection()[0])
+
+
+def AddContact():
+    contact_list.append([Name.get(), Number.get()])
+    Select_set()
+
+
+def EDIT():
+    contact_list[Selected()] = [Name.get(), Number.get()]
+    Select_set()
+
+
+def DELETE():
+    del contact_list[Selected()]
+    Select_set()
+
+
+def VIEW():
+    NAME, PHONE = contact_list[Selected()]
+    Name.set(NAME)
+    Number.set(PHONE)
+
+
+def EXIT():
+    root.destroy()
+
+
+def RESET():
+    Name.set('')
+    Number.set('')
+
+
+def Select_set():
+    contact_list.sort()
+    select.delete(0, END)
+    for name, phone in contact_list:
+        select.insert(END, name)
+
+
+Select_set()
