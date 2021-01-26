6dc1efa8eaf2ff74aa0f20547d8e18f79508f3ac
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-26 18:16:47 +0530

    Winning function

--- abc.py
+++ abc.py
@@ -1,11 +1,30 @@
 import tkinter as tk
 from tkinter import *
+import tkinter.messagebox as msg
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+mark = '' ``
+count = 0
+panels = ["panel"]*10
+
+
+def win(panels, sign):
+    return((panels[1] == panels[2] == panels[3] == sign)
+           or (panels[1] == panels[4] == panels[7] == sign)
+           or (panels[1] == panels[5] == panels[9] == sign)
+           or (panels[2] == panels[5] == panels[8] == sign)
+           or (panels[3] == panels[6] == panels[9] == sign)
+           or (panels[3] == panels[5] == panels[7] == sign)
+           or (panels[4] == panels[5] == panels[6] == sign)
+           or (panels[7] == panels[8] == panels[9] == sign))
+
 
 window = tk.Tk()
 
 window.geometry("500x600")
 
-window.title("New Project")
+window.title("TIC-TAC-TOE")
 
 
 def Hello():
