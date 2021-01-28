e7d4f592826afd1b89199c9c1cabc1679c5f8457
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-28 13:54:16 +0530

    Code completed

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import *
 import tkinter.messagebox as msg
 
+
+root = tk.Tk()
+
+root.geometry("500x600")
+
+root.title("TIC-TAC-TOE")
 digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 mark = '' ``
@@ -189,16 +195,34 @@ def checker(digit):
         root.destroy()
 
 
-window = tk.Tk()
+Label(root, text="player1: X", font="times 15").grid(row=0, column=1)
+Label(root, text="player1: O", font="times 15").grid(row=0, column=2)
 
-window.geometry("500x600")
-
-window.title("TIC-TAC-TOE")
-
-
-def Hello():
-    print("Hello world")
-
-
-button = Button(window, text="show hello", command="Hello").place(x=30, y=150)
-window.mainloop()
+button1 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(1))
+button1.grid(row=1, column=1)
+button2 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(2))
+button2.grid(row=1, column=2)
+button3 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(3))
+button3.grid(row=1, column=3)
+button4 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(4))
+button4.grid(row=2, column=1)
+button5 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(5))
+button5.grid(row=2, column=2)
+button6 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(6))
+button6.grid(row=2, column=3)
+button7 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(7))
+button7.grid(row=3, column=1)
+button8 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(8))
+button8.grid(row=3, column=2)
+button9 = Button(root, width=15, height=7, font=(
+    'Times 16 bold'), command=lambda: checker(9))
+button9.grid(row=3, column=3)
+root.mainloop()
