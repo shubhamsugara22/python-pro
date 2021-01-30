07054b2ca9692c98b1e4c676c3c03405dc6bac4c
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-30 19:16:32 +0530

    ...

--- handwritten.py
+++ handwritten.py
@@ -6,3 +6,21 @@ from keras.layers import Conv2D, MaxPooling2D
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape, y_train.shape)
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# converting class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+
+print('x_train shape :', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
