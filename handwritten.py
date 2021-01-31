71ee42f6653fcec56e0ed9fd6ebe5c7d7eea6928
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-31 19:32:19 +0530

    model creation

--- handwritten.py
+++ handwritten.py
@@ -24,3 +24,29 @@ x_test /= 255
 print('x_train shape :', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten)
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+
+hist = model.fit(x_train, y_train, batch_size=batch_size,
+                 epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+
+print("The model has been successfully trained")
+
+model.save('mnist.h5')
+print('saving the model as mnist.h5')
