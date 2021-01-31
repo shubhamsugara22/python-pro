573ec8d07029923fb31f7692e871d9b0792a99f0
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-01-31 20:52:02 +0530

    Model Created

--- handwritten/handwritten.py
+++ handwritten/handwritten.py
@@ -4,11 +4,16 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
+
+batch_size = 128
+num_classes = 10
+epochs = 10
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape, y_train.shape)
 
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
 
 # converting class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -25,9 +30,6 @@ print('x_train shape :', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-batch_size = 128
-num_classes = 10
-epochs = 10
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -35,7 +37,6 @@ model.add(Conv2D(32, kernel_size=(3, 3),
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-model.add(Flatten)
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
