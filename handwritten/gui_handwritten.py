df051ae85b8a3f24b0e3c2891a6afcb2564b022c
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-02 17:52:49 +0530

    gui part 1

--- handwritten/gui_handwritten.py
+++ handwritten/gui_handwritten.py
@@ -5,3 +5,20 @@ import win32gui
 from PIL import ImageGrab, Image
 
 model = Load_model('mnist.h5')
+
+
+def predict_digit(img):
+    img = img.resize((28, 28))
+    img = img.convert('L')
+    img = np.array(img)
+    img = img.reshape(1, 28, 28, 1)
+    img = img/255.0
+    res = model.predict([img])[0]
+    return np.argmax(res), max(res)
+
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.x = self.y = 0
