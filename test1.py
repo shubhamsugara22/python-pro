f76019dfc44f789fbd1a38f4f260dd9a50ebce04
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-09 19:26:00 +0530

    Add input

--- test1.py
+++ test1.py
@@ -9,6 +9,14 @@ Label(root, text="click any one :", font='arial 15 bold').place(x=40, y=80)
 
 
 def madlib1():
+    animals = input("enter animal name")
+    profession = input("enter a profession name")
+    cloth = input("enter a piece of cloth ")
+    things = input("enter thing name")
+    name = input("enter a person name")
+    place = input("enter a place name")
+    verb = input("enter a verb ing form")
+    food = input("enter favourite food")
 
 
 def madlib2():
