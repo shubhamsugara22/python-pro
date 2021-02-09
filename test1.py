9c8cfe6b2ce0e35f137ce56fddb9708a6c678855
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-09 19:21:32 +0530

    madlib func

--- test1.py
+++ test1.py
@@ -4,3 +4,14 @@ from tkinter import *
 root = Tk()
 root.geometry("300x300")
 root.title("madlibs generator")
+Label(root, text="mad flair geenrator have fun ", font='arial 20 bold').pack()
+Label(root, text="click any one :", font='arial 15 bold').place(x=40, y=80)
+
+
+def madlib1():
+
+
+def madlib2():
+
+
+def madlib3():
