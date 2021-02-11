90ade3de1443456726e14c8488665315f0bf5354
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-11 17:26:57 +0530

    input function complete

--- madlibs.py
+++ madlibs.py
@@ -31,8 +31,25 @@ def madlib2():
     insect = input("enter a insect name:")
     food = input("enter a food name")
     verb = input("enter  a verb")
-    print('Last night I dreamed I was a ' + adjactive + ' butterfly with ' + color + ' splocthes that looked like '+thing + ' .I flew to ' + place + ' with my bestfriend and '+person +
-          ' who was a '+adjactive1 + ' ' + insect + ' .We ate some ' + food + ' when we got there and then decided to '+verb + ' and the dream ended when I said-- lets ' + verb + '.')
+    print('Last night I dreamed I was a ' + adjective + ' butterfly with ' + color + ' splocthes that looked like '+thing + ' .I flew to ' + place + ' with my bestfriend and '+person +
+          ' who was a '+adjective1 + ' ' + insect + ' .We ate some ' + food + ' when we got there and then decided to '+verb + ' and the dream ended when I said-- lets ' + verb + '.')
 
 
 def madlib3():
+    person = input("enter person name")
+    color = input("enter color:")
+    foods = input("enter food name")
+    adjective = input("enter as adjective name")
+    thing = input("enter a thing name")
+    place = input("enter a place")
+    verb = input("enter verb")
+    adverb = input("enter adverb")
+    food = input("enter the food name")
+    things = input("enter a thing name")
+    print('Today we picked apple from '+person + "'s Orchard. I had no idea there were so many different varieties of apples. I ate " + color + ' apples straight off the tree that tested like '+foods + '. Then there was a '+adjective + ' apple that looked like a ' + thing +
+          '.When our bag were full, we went on a free hay ride to '+place + ' and back. It ended at a hay pile where we got to ' + verb + ' ' + adverb + '. I can hardly wait to get home and cook with the apples. We are going to make appple '+food + ' and '+things+' pies!.')
+
+
+Button()
+Button()
+Button()
