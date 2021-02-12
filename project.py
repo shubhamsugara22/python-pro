ee12f0ca9b250fd42f98194493ae08b0f74304c7
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-12 19:22:19 +0530

    open cv

--- project.py
+++ project.py
@@ -1,2 +1,8 @@
 import tkinter as tk
 from tkinter import *
+import cv2 as cv
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="video path")
+args = vars(ap.parse_args())
