ef541077fffd56a6b3a03c9844d78dd28e13f315
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-14 16:52:29 +0530

    code completed

--- shape_detector.py
+++ shape_detector.py
@@ -3,6 +3,7 @@ from tkinter import *
 import cv2
 from cv2 import *
 import argparse
+from argparse import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="video path")
@@ -26,3 +27,18 @@ while True:
 
 for cnt in cnts:
     approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+
+if len(approx) == 5:
+    cv2.drawContours(frame, [approx], -1, (0, 0, 255), 4)
+    status = "Target(s) in sight!"
+
+    cv2.putText(frame, status, (20, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('s'):
+        break
+
+camVideo.release()
+cv2.destroyAllWindows()
