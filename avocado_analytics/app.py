b0ed8e3d2b75abe79e82400ff3a6684d72284646
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-16 14:39:48 +0530

    dashboard created

--- avocado_analytics/app.py
+++ avocado_analytics/app.py
@@ -3,4 +3,47 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
+
+# intial data queried for dashboard
 data = pd.read_csv("avocado.csv")
+data = data.query("type == 'conventional' and region == 'Albany'")
+data['Date'] = pd.to_datetime(data['Date'], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="avocado analytics"),
+        html.P(
+            children="Analyze the behavior of avocado prices  and number of avocados sold in the US between 2015  and 2018"
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y":data["AveragePrice"],
+                        "type":"lines",
+                    },
+                ],
+                "layout":{"title": "Average price of avocados"}
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y":data["Total Volume"],
+                        "type":"lines",
+                    },
+                ],
+                "layout":{"title": "Avocado sold"}
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
