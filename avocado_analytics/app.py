b7d07f9385f30189a49ca86f0a01fc5ce64caf8d
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-18 23:59:48 +0530

    callback added

--- avocado_analytics/app.py
+++ avocado_analytics/app.py
@@ -41,49 +41,56 @@ app.layout = html.Div(
                 html.Div(
                     children=[
                         html.Div(children="Region", className="menu-title"),
-                dcc.Dropdown(
-                    id="region-filter",
-                    options=[
-                        {"label": region, "value": region}
-                        for region in np.sort(data.region.unique())
-                    ],
-                    value="Albany",
-                    clearable=False,
-                    className="dropdown",
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in np.sort(data.region.unique())
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        )
+                    ]
                 )
-            ]
-        )
-
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {"label": avocado_type, "value": avocado_type}
+                                for avocado_type in np.sort(data.region.unique())
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Date Range",
+                                 className="menu-title"),
+                        dcc.DatePickerRange(
+                            id="data-range",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date(),
+                        ),
+                    ],
+                ),
+            ],
+            className="menu",
+        ),
         html.Div(
             children=[
                 html.Div(
                     children=dcc.Graph(
                         id="price-chart",
                         config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": data["Date"],
-                                    "y":data["AveragePrice"],
-                                    "type":"lines",
-                                    "hovertemplate":"$%{y:.2f}"
-                                                    "<extra></extra>",
-                                },
-                            ],
-                            "layout":{
-                                "title": {
-                                    "text": "Average price of avocados",
-                                    "x": 0.05,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {
-                                    "tickprefix": "$",
-                                    "fixedrange": True,
-                                },
-                                "colorway": ["#17B897"],
-                            },
-                        },
                     ),
                     className="card",
                 ),
@@ -91,34 +98,78 @@ app.layout = html.Div(
                     children=dcc.Graph(
                         id="volume-chart",
                         config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": data["Date"],
-                                    "y":data["Total Volume"],
-                                    "type":"lines",
-                                },
-                            ],
-                            "layout":
-                            {
-                                "title": {
-                                    "text": "Avocado sold",
-                                    "x": 0.05,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {"fixedrange": True},
-                                "colorway": ["#E12D39"],
-                            },
-                        },
                     ),
                     className="card",
                 ),
             ],
-            className="wrapper"
-        )
+            className="wrapper",
+        ),
     ]
 )
+
+
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("region-filter", "value"),
+        Input("type-filter", "Value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ]
+)
+def update_charts(region, avocado_type, start_date, end_date):
+    mask = (
+        (data.region == region)
+        & (data.type == avocado_type)
+        & (data.Date >= start_date)
+        & (data.Date <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": data["Date"],
+                "y":data["AveragePrice"],
+                "type":"lines",
+                "hovertemplate":"$%{y:.2f}"
+                "<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average price of avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {
+                "tickprefix": "$",
+                "fixedrange": True,
+            },
+            "colorway": ["#17B897"],
+        },
+    }
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": data["Date"],
+                "y":data["Total Volume"],
+                "type":"lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Avocado sold",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    },
+    return price_chart_figure, volume_chart_figure
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
