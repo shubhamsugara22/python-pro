f4ed399dd106a16286c66f2e430aabc9957474c8
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-03 13:23:46 +0530

    added selenium webdriver

--- selenium/test.py
+++ selenium/test.py
@@ -2,10 +2,22 @@ import os
 import pathlib
 import unittest
 from selenium import webdriver
+# same can be used for firefox and ie
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(
+    executable_path='C:/Users/hp/Downloads/chromedriver_win32/chromedriver.exe')
+
+
+# class WebPageTest(unittest.TestCase):
+
+#     def test_title:
+
+#     def test_increase:
+
+#     def test_decrease:
