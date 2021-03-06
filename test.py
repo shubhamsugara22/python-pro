60f785e9946d1422784ed59bc8ef04011dbd7d5c
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-06 23:16:29 +0530

    new scraper

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import numpy as np
 import pandas as pd
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+url = "https://towardsdatascience.com/top-10-in-demand-web-development-frameworks-in-2021-8a5b668be0d6"
