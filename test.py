fb5fdf738b64d0d7152057c04b4ea18e06e642f6
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-07 19:31:14 +0530

    new url needed

--- test.py
+++ test.py
@@ -5,3 +5,12 @@ from bs4 import BeautifulSoup
 
 
 url = "https://towardsdatascience.com/top-10-in-demand-web-development-frameworks-in-2021-8a5b668be0d6"
+
+page = urlopen(url).read()
+
+html = page.read().decode("utf-8")
+
+soup = BeautifulSoup(html, "html-parser")
+
+content = soup.find(id="main")
+print(content)
