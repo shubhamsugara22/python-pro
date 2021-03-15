c3a5d5c134598af05a4197ae2bde77c5637eb115
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-15 10:27:13 +0530

    added event handler

--- speed-type/game.py
+++ speed-type/game.py
@@ -74,37 +74,85 @@ class Game:
 
             print(self.results)
             pygame.display.update()
-    
-    
+
     def reset_game(self):
-        self.screen.blit(self.open_img,(0,0))
+        self.screen.blit(self.open_img, (0, 0))
 
         pygame.display.update()
         time.sleep(1)
-        
-        self.reset= False
-        self.end= False
+
+        self.reset = False
+        self.end = False
 
         self.input_text = ''
-        self.word=''
+        self.word = ''
         self.time_start = 0
-        self.total_time= 0
+        self.total_time = 0
         self.wpm = 0
 
-        self.word =self.get_sentence()
-        if(not self.word):self.reset_game()
+        self.word = self.get_sentence()
+        if(not self.word):
+            self.reset_game()
 
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.bg,(0,0))
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(self.bg, (0, 0))
 
-        msg= "typing speed test"
-        self.draw_test(self.screen, msg,80,80,self.HEAD_C)
-        pygame.draw.rect(self.screen,(255,192,25),(50,250,650,50),2)
+        msg = "typing speed test"
+        self.draw_test(self.screen, msg, 80, 80, self.HEAD_C)
+        pygame.draw.rect(self.screen, (255, 192, 25), (50, 250, 650, 50), 2)
 
-        self.draw_text(self.screen,self.word,200,28,self.TEXT_C)
+        self.draw_text(self.screen, self.word, 200, 28, self.TEXT_C)
 
         pygame.display.update()
 
-
     def run(self):
         self.reset_game()
+
+        self.running = True
+        while(self.running):
+            clock = pygame.time.clock()
+            self.screen.fill((0, 0, 0), (50, 250, 650, 50), 2)
+            pygame.draw.rect(self.screen, self.HEAD_C, (50, 250, 650, 50), 2)
+
+            self.draw_text(self.screen, self.input_text,
+                           274, 26, (250, 250, 250))
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    x, y = pygame.mouse.get_pos()
+                    if(x >= 50 and x <= 650 and y >= 250 and y <= 300):
+                        self.active = True
+                        self.input_text = ''
+                        self.time_start = time.time()
+                    if(x >= 310 and x <= 510 and y >= 390 and self.end):
+                        self.reset_game()
+                        x, y = pygame.mouse.get_pos()
+
+                elif event.type == pygame.KEYDOWN:
+                    if self.active and self.end:
+                        if event.key == pygame.K_RETURN:
+                            print(self.input_text)
+                            self.show_results(self.screen)
+                            print(self.results)
+                            self.draw_text(
+                                self.screen, self.results, 350, 28, self.RESULT_C)
+                            self.end = True
+
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.input_text = self.input_text[:-1]
+                        else:
+                            try:
+                                self.input_text + =event.unicode
+                            except:
+                                pass
+
+        pygame.display.update()
+
+    clock.tick(60)
+
+
+Game.run()
