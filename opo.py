116253d65d818218068a68678dc552ac2aa04a8c
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-02 22:32:58 +0530

    video downloader

---
+++ opo.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from pytube import Youtube
+
+
+root = Tk()
+root.geometry("500X400")
+root.resizable(0, 0)
+root.title("video downloader")
+
+Label(root, text="Video downloader", font="arial 20 bold").pack()
