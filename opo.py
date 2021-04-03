7e7f4dc515e892cb934651400135c68c79ada867
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-03 23:26:32 +0530

    created field to enter link

--- opo.py
+++ opo.py
@@ -8,3 +8,7 @@ root.resizable(0, 0)
 root.title("video downloader")
 
 Label(root, text="Video downloader", font="arial 20 bold").pack()
+
+link = StringVar()
+Label(root, text="paste link here", font='arial 15 bold').place(x=160, y=60)
+link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
