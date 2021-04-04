8ce0c6a37ab1063c45f10b85c2410d2be48384f3
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-04 23:23:06 +0530

    added download function

--- opo.py
+++ opo.py
@@ -12,3 +12,15 @@ Label(root, text="Video downloader", font="arial 20 bold").pack()
 link = StringVar()
 Label(root, text="paste link here", font='arial 15 bold').place(x=160, y=60)
 link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
+
+
+def Downloader():
+    url = Youtube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text="DOWNLOADED", font="arial 15").place(x=180, y=210)
+
+
+Button(root, text="DOWNLOAD", font="arial 15 bold", bg='pale violet red',
+       padx=2, command=Downloader).place(x=180, y=150)
+root.mainloop()
