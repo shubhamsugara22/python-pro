d5d57fa179725d9c1c3e8303a644400bd95962c7
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-06 22:39:17 +0530

    added initialized window

--- Jump/key.py
+++ Jump/key.py
@@ -2,3 +2,13 @@ import pygame
 import random
 import time
 pygame.init()
+WIDTH = 800
+HEIGHT = 600
+black = (0, 0, 0)
+
+gameDisplay = pygame.display.set_mode(WIDTH, HEIGHT)
+
+background = pygame.image.loadI("")
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+font = pygame.font.Font("comic.ttf", 40)
