7fa140d7b53104654eda4abfa7b30686c046cea9
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-07 11:40:17 +0530

    functions added

--- Jump/key.py
+++ Jump/key.py
@@ -12,3 +12,19 @@ background = pygame.image.loadI("")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 font = pygame.font.Font("comic.ttf", 40)
+
+word_speed = 0.5
+score = 0
+
+
+def new_word():
+    global displayword, yourword, x_cor, y_cor, text, word_speed
+    x_cor = random.randint(300, 700)
+    y_cor = 200
+    word_speed += 0.10
+    yourword = ''
+    words = open("words.txt").read().split(',')
+    displayword = random.choice(words)
+
+
+new_word()
