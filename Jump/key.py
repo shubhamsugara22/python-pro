09426af106e719889aacd8e601d9a659eb5ecccd
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-08 15:54:48 +0530

    working keyboard game

--- Jump/key.py
+++ Jump/key.py
@@ -6,7 +6,7 @@ WIDTH = 800
 HEIGHT = 600
 black = (0, 0, 0)
 
-gameDisplay = pygame.display.set_mode(WIDTH, HEIGHT)
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Keyboard Jump Game')
 background = pygame.image.load("keyback.jpg")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
@@ -33,7 +33,7 @@ font_name = pygame.font.match_font('comic.ttf')
 
 def draw_text(display, text, size, x, y):
     font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, true, black)
+    text_surface = font.render(text, True, black)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     gameDisplay.blit(text_surface, text_rect)
@@ -70,14 +70,15 @@ while True:
     character = pygame.image.load('char.jpg')
     character = pygame.transform.scale(character, (50, 50))
     wood = pygame.image.load('wood-.png')
-    wood = pygame.transform.sclae(wood, (90, 50))
+    wood = pygame.transform.scale(wood, (90, 50))
+
     gameDisplay.blit(background, (0, 0))
+    y_cor += word_speed
+
     gameDisplay.blit(wood, (x_cor-50, y_cor+15))
     gameDisplay.blit(character, (x_cor-100, y_cor))
     draw_text(gameDisplay, str(displayword), 40, x_cor, y_cor)
     draw_text(gameDisplay, 'Score:'+str(score), 40, WIDTH/2, 5)
-
-    y_cor += word_speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -95,7 +96,7 @@ while True:
                 time.sleep(2)
                 pygame.quit()
 
-    if y_cor < HEIGHT - 5:
+    if y_cor < HEIGHT-5:
         pygame.display.update()
     else:
         game_front_screen()
