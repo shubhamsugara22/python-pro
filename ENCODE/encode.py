71bf13eeb6bbdc71351252315e0e859d705218e0
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-09 11:37:29 +0530

    display and variable added

--- ENCODE/encode.py
+++ ENCODE/encode.py
@@ -1,2 +1,17 @@
 from tkinter import *
 import base64
+
+
+root = Tk()
+root.geometry("500x300")
+root.resizable(0, 0)
+root.title("Message encoder and decoder")
+
+
+Label(root, text="ENCODE DECODE", font='arial 20 bold').pack()
+Label(root, text="Shubham project", font='arial 20 bold').pack(side=BOTTOM)
+
+Text = StringVar()
+private_key = StringVar()
+mode = StringVar()
+Result = StringVar()
