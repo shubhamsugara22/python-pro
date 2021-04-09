bb8dc5292976f85f85135d94643568fdcb7d8748
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-09 11:41:48 +0530

    added encode function

--- ENCODE/encode.py
+++ ENCODE/encode.py
@@ -15,3 +15,13 @@ Text = StringVar()
 private_key = StringVar()
 mode = StringVar()
 Result = StringVar()
+
+
+def Encode(key, message):
+    enc = []
+
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i])+ord(key_c)) % 256))
+
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
