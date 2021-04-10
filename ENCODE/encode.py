71602000d8fc1f12172106edd8140fc1f20d9494
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-04-10 17:16:35 +0530

    decode function added

--- ENCODE/encode.py
+++ ENCODE/encode.py
@@ -25,3 +25,14 @@ def Encode(key, message):
         enc.append(chr((ord(message[i])+ord(key_c)) % 256))
 
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+
+def Decode(key, message):
+    dec = []
+    message = base64.urlsafe_b64decode(message).decode()
+
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        dec.append(chr(256+ord(message[i])-ord(key_c)) % 256)
+
+    return " ".join(dec)
